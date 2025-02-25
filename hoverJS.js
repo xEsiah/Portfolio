2527dc540ffbcd1268bbb2b66124f88e43c24edc
Dupré <theo.thws.06@gmail.com>
2025-02-25 21:29:52 +0100

    Ajustement CSS, connexion entre Page pres eCommerc et page principale & ajout de scripts sur hover de la compétence javascript

---
+++ hoverJS.js
@@ -0,0 +1,9 @@
+function showSmiley() {
+  const CompetenceJS = document.getElementById("JS");
+  CompetenceJS.innerHTML = "Je n'ai pas menti 😎";
+}
+
+function resetText() {
+  const CompetenceJS = document.getElementById("JS");
+  CompetenceJS.innerHTML = "JavaScript ⚡";
+}
