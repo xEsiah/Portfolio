9eccd8804686bac653162b36a11bbf8f123a3449
Dupré <theo.thws.06@gmail.com>
2025-02-27 00:54:19 +0100

    implémentation de certains scripts sur mobile annulée

--- hoverJS.js
+++ hoverJS.js
@@ -13,39 +13,3 @@ document.querySelectorAll("a.project-card").forEach((link) => {
     window.location.href = link.href;
   });
 });
-// Ajouter les effets de survol sur les éléments touchés sur mobile
-document.querySelectorAll(".project-card").forEach((card) => {
-  card.addEventListener("touchstart", () => {
-    card.classList.add("hover");
-  });
-  card.addEventListener("touchend", () => {
-    card.classList.remove("hover");
-  });
-});
-
-document.querySelectorAll("#competences li").forEach((item) => {
-  item.addEventListener("touchstart", () => {
-    item.classList.add("hover");
-  });
-  item.addEventListener("touchend", () => {
-    item.classList.remove("hover");
-  });
-});
-
-document.querySelectorAll("#centrer_retour_index").forEach((link) => {
-  link.addEventListener("touchstart", () => {
-    link.classList.add("hover");
-  });
-  link.addEventListener("touchend", () => {
-    link.classList.remove("hover");
-  });
-});
-
-document.querySelectorAll(".liens_footer").forEach((link) => {
-  link.addEventListener("touchstart", () => {
-    link.classList.add("hover");
-  });
-  link.addEventListener("touchend", () => {
-    link.classList.remove("hover");
-  });
-});
